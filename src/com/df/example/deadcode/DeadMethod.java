5ad0d0eb3044e640bbe06800c35000c9366e4763
Kalbal <kashyap.albal@aurea.com>
2017-03-02 16:06:47 +0530

    Added couple of examples

---
+++ src/com/df/example/deadcode/DeadMethod.java
@@ -0,0 +1,15 @@
+package com.df.example.deadcode;
+
+public class DeadMethod {
+
+    private String indirectDeadVar3;
+    private String goodVar4;
+    
+    public String goodMethod(){
+        return goodVar4;
+    }
+    
+    public String deadMethod(){
+        return indirectDeadVar3;
+    }
+}
